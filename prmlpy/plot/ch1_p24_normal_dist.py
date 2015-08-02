1e227b29d81b0066f0bf5d326f4330ff8acf716c
nakatani.sho <nakatani.sho@dena.jp>
2015-08-02 17:01:44 +0900

    グローバル変数を排除

--- prmlpy/plot/ch1_p24_normal_dist.py
+++ prmlpy/plot/ch1_p24_normal_dist.py
@@ -20,32 +20,6 @@ from matplotlib import gridspec
 from prmlpy.distribution.normal import normal_dist
 
 
-# global vars
-_widgets = {
-    'sliders': {},
-}
-line2d = None  # mtaplotlib.pyplot.plot() の返り値。ウィジェットのコールバックで再設定する。
-
-# [TODO] - あとで widgets で一般化
-axfreq = None
-slider_mu = None
-
-
-
-def update_plot(_):
-    """
-    matplotlib.widgets.*.on_changed に渡すコールバック関数。
-
-    :param _:  matplotlib.widgets.*.on_changed のコールバックがセットする値だが、使わない。
-    :return: None
-    """
-    mu = slider_mu.val
-    x_list = np.arange(0.0, 1.0, 0.001)
-    y_list = normal_dist(x_list, mu, 0.2)
-    global line2d
-    line2d.set_ydata(y_list)
-
-
 def init_figure():
     """
     グラフ領域の横にウィジェットが並ぶ、典型的な図領域を作成する。
@@ -68,7 +42,17 @@ def init_figure():
 
 
 def main():
-    global line2d, axfreq, slider_mu
+    def update_plot(_):
+        """
+        matplotlib.widgets.*.on_changed に渡すコールバック関数。
+
+        :param _:  matplotlib.widgets.*.on_changed のコールバックがセットする値だが、使わない。
+        :return: None
+        """
+        mu = slider_mu.val
+        x_list = np.arange(0.0, 1.0, 0.001)
+        y_list = normal_dist(x_list, mu, 0.2)
+        line2d.set_ydata(y_list)
 
     ax_graph, ax_slider0 = init_figure()
 
@@ -77,7 +61,6 @@ def main():
 
     line2d, = ax_graph.plot(x_list, y_list)
 
-    #axfreq = plot.axes([0.25, 0.1, 0.65, 0.03])
     slider_mu = widgets.Slider(ax_slider0, 'mu', 0.0, 1.0, valinit=0.5)
     slider_mu.on_changed(update_plot)
 
