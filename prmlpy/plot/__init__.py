1e92a34049f54a9dab472e10628f389555f1f3dc
Sho Nakatani <lay.sakura@gmail.com>
2015-08-02 20:52:28 +0900

    init_figure() を共通関数にした

--- prmlpy/plot/__init__.py
+++ prmlpy/plot/__init__.py
@@ -6,3 +6,37 @@
 
     matplotlib を使った可視化系のプログラム群。実行可能なものが揃っている。
 """
+
+# standard modules
+
+# 3rd party modules
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+
+# original modules
+
+
+def init_figure(slider_list=[]):
+    """グラフ領域の横にウィジェットが並ぶ、典型的な図領域を作成する。
+
+    配置は GridSpec ( http://matplotlib.org/users/gridspec.html ) を使用して調整。
+
+    :param slider_list: スライダーバー情報のリスト。
+    :type sliders: list of prmlpy.widgetwrapper.Slider
+    :return: (ax_graph, ax_slider_list)
+    """
+    MAX_SLIDERS = 10
+
+    # グラフ領域
+    gs_graph = gridspec.GridSpec(1, 1)
+    gs_graph.update(left=0.00, right=0.80, wspace=0.05)
+    ax_graph = plt.subplot(gs_graph[:, :])
+
+    # スライダーバー領域 (スライダーバーは最大10本まで入る)
+    gs_slider = gridspec.GridSpec(MAX_SLIDERS, 1)
+    gs_slider.update(left=0.80, right=1.00, wspace=0.05)
+    ax_slider_list = []
+    for i, slider in enumerate(slider_list):
+        ax_slider_list.append(plt.subplot(gs_slider[i, :]))
+
+    return ax_graph, ax_slider_list
--- prmlpy/plot/ch1_p24_normal_dist.py
+++ prmlpy/plot/ch1_p24_normal_dist.py
@@ -12,38 +12,12 @@
 
 # 3rd party modules
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import gridspec
 
 # original modules
+from matplotlib import pyplot as plt
 from prmlpy.distribution.normal import normal_dist
 from prmlpy.widgetwrapper.slider import SliderWrapper
-
-
-def init_figure(slider_list=[]):
-    """グラフ領域の横にウィジェットが並ぶ、典型的な図領域を作成する。
-
-    配置は GridSpec ( http://matplotlib.org/users/gridspec.html ) を使用して調整。
-
-    :param slider_list: スライダーバー情報のリスト。
-    :type sliders: list of prmlpy.widgetwrapper.Slider
-    :return: (ax_graph, ax_slider_list)
-    """
-    MAX_SLIDERS = 10
-
-    # グラフ領域
-    gs_graph = gridspec.GridSpec(1, 1)
-    gs_graph.update(left=0.00, right=0.80, wspace=0.05)
-    ax_graph = plt.subplot(gs_graph[:, :])
-
-    # スライダーバー領域 (スライダーバーは最大10本まで入る)
-    gs_slider = gridspec.GridSpec(MAX_SLIDERS, 1)
-    gs_slider.update(left=0.80, right=1.00, wspace=0.05)
-    ax_slider_list = []
-    for i, slider in enumerate(slider_list):
-        ax_slider_list.append(plt.subplot(gs_slider[i, :]))
-
-    return ax_graph, ax_slider_list
+import prmlpy.plot
 
 
 def main():
@@ -65,7 +39,7 @@ def main():
     slider_mu = SliderWrapper(label='mu', min_val=0.0, max_val=1.0, init_val=INIT_MU)
     slider_sigma = SliderWrapper(label='sigma', min_val=0.01, max_val=0.10, init_val=INIT_SIGMA)
 
-    ax_graph, (ax_slider_mu, ax_slider_sigma) = init_figure([slider_mu, slider_sigma])
+    ax_graph, (ax_slider_mu, ax_slider_sigma) = prmlpy.plot.init_figure([slider_mu, slider_sigma])
 
     slider_mu.instantiate(axis=ax_slider_mu, on_changed=update_plot)
     slider_sigma.instantiate(axis=ax_slider_sigma, on_changed=update_plot)
